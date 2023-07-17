9d5f564f8737f503c1bc01b8555e4aace4fedd07
Серкес Олександра Василівна <SerkesOV@comfy.ua>
2023-07-17 21:47:52 +0300

    17/07/2023
    Верстка для Karcher AF20

--- Sasha_code/Karcher AF 20/app/js/script.js
+++ Sasha_code/Karcher AF 20/app/js/script.js
@@ -3,8 +3,8 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.slider-buton-next',
+        prevEl: '.slider-button-prev',
     },
     pagination: {
         el: '.swiper-pagination',
@@ -15,21 +15,5 @@ const swiper = new Swiper('.swiper', {
     fadeEffect: {
         crossFade: true
     },
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     320: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 20
-    //     },
-    //     // when window width is >= 480px
-    //     480: {
-    //         slidesPerView: 3,
-    //         spaceBetween: 30
-    //     },
-    //     // when window width is >= 640px
-    //     640: {
-    //         slidesPerView: 4,
-    //         spaceBetween: 40
-    //     }
-    // }
+    
 });
