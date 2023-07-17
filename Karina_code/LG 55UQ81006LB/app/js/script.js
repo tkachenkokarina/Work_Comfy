dbe6e34209ce3112879f904b718b876727776745
Karina Tkachenko <dyukovakarina@gmail.com>
2023-07-17 15:29:40 +0300

    17.07.2023

---
+++ Karina_code/LG 55UQ81006LB/app/js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+});
