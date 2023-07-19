5d2a04f9f4daae10172d00b67b0f3a08152d2673
Karina Tkachenko <dyukovakarina@gmail.com>
2023-07-19 11:13:08 +0300

    19.07.2023

---
+++ Karina_code/Samsung QE55QN95BAUXUA/app/js/script.js
@@ -0,0 +1,175 @@
+let myPicSlider = new Swiper('.picture-samsungtv__slider', {
+    speed: 500,
+    // slidesPerView: 1,
+    // spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next, picture-samsungtv-button-next',
+        prevEl: '.swiper-button-prev, picture-samsungtv-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    // effect: 'fade',
+    // fadeEffect: {
+    //     crossFade: true
+    // },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        479.98: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        601: {
+            slidesPerView: 1.5,
+            spaceBetween: 35
+        },
+        767.98: {
+            slidesPerView: 1.5,
+            spaceBetween: 60
+        },
+        // when window width is >= 640px
+        1125: {
+            slidesPerView: 1.5,
+            spaceBetween: 80
+        }
+    }
+});
+
+let myGamesSlider = new Swiper('.games-samsungtv__slider', {
+    speed: 500,
+    // slidesPerView: 1,
+    // spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next, picture-samsungtv-button-next',
+        prevEl: '.swiper-button-prev, picture-samsungtv-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    // effect: 'fade',
+    // fadeEffect: {
+    //     crossFade: true
+    // },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        479.98: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        601: {
+            slidesPerView: 1.5,
+            spaceBetween: 35
+        },
+        767.98: {
+            slidesPerView: 1.5,
+            spaceBetween: 60
+        },
+        // when window width is >= 640px
+        1125: {
+            slidesPerView: 1.5,
+            spaceBetween: 80
+        }
+    }
+});
+
+let mySoundSlider = new Swiper('.sound-samsungtv__slider', {
+    speed: 500,
+    // slidesPerView: 1,
+    // spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next, picture-samsungtv-button-next',
+        prevEl: '.swiper-button-prev, picture-samsungtv-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    // effect: 'fade',
+    // fadeEffect: {
+    //     crossFade: true
+    // },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        479.98: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        601: {
+            slidesPerView: 1.5,
+            spaceBetween: 35
+        },
+        767.98: {
+            slidesPerView: 1.5,
+            spaceBetween: 60
+        },
+        // when window width is >= 640px
+        1125: {
+            slidesPerView: 1.5,
+            spaceBetween: 80
+        }
+    }
+});
+
+let mySmartSlider = new Swiper('.smart-samsungtv__slider', {
+    speed: 500,
+    // slidesPerView: 1,
+    // spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next, picture-samsungtv-button-next',
+        prevEl: '.swiper-button-prev, picture-samsungtv-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    // effect: 'fade',
+    // fadeEffect: {
+    //     crossFade: true
+    // },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        479.98: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        601: {
+            slidesPerView: 1.5,
+            spaceBetween: 35
+        },
+        767.98: {
+            slidesPerView: 1.5,
+            spaceBetween: 60
+        },
+        // when window width is >= 640px
+        1125: {
+            slidesPerView: 1.5,
+            spaceBetween: 80
+        }
+    }
+});
