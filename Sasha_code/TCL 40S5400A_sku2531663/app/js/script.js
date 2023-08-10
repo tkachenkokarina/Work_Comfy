4cfccf69900c55111e06ccf13baf10c4f570c46e
Серкес Олександра Василівна <SerkesOV@comfy.ua>
2023-08-10 13:26:35 +0300

    10/08/2023
    Верстка для TCL 40S5400A (sku2531663)

---
+++ Sasha_code/TCL 40S5400A_sku2531663/app/js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    
+});
