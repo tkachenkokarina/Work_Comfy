75b3c9127d418b7ee57e6eab80f4b9702395a5ce
Ткаченко Карина Олексіївна <dyukovakarina@gmail.com>
2023-08-21 16:16:38 +0300

    21,08,2023

---
+++ Karina_code/Телевизор Samsung QE43Q60CAUXUA/app/js/script.js
@@ -0,0 +1,95 @@
+let myQualitySlider = new Swiper('.quality-samsungtv__slider', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 60,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            spaceBetween: 20
+        },
+        601: {
+            spaceBetween: 60
+        }
+    }
+});
+
+let mySoundSlider = new Swiper('.sound-samsungtv__slider', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 60,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            spaceBetween: 20
+        },
+        601: {
+            spaceBetween: 60
+        }
+    }
+});
+
+let myDesignSlider = new Swiper('.design-samsungtv__slider', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 60,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            spaceBetween: 20
+        },
+        601: {
+            spaceBetween: 60
+        }
+    }
+});
+
+let mySpecSlider = new Swiper('.spec-samsungtv__slider', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 60,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            spaceBetween: 20
+        },
+        601: {
+            spaceBetween: 60
+        }
+    }
+});
