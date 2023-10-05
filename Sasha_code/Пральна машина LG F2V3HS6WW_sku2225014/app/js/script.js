9f0745ede34c0c416146626ec17195734ff0dff6
Серкес Олександра Василівна <SerkesOV@comfy.ua>
2023-10-05 16:36:25 +0300

    05/10/2023
    Пральна машина LG F2V3HS6WW_sku2225014

---
+++ Sasha_code/Пральна машина LG F2V3HS6WW_sku2225014/app/js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    speed: 300,
+    slidesPerView: 1,
+    spaceBetween: 10,    
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    
+});
