98b25d7c9e4a8a05e8fd0cf0c630181cd97280ec
Серкес Олександра Василівна <SerkesOV@comfy.ua>
2023-10-26 16:55:56 +0300

    26/10/2023
    Samsung NZ64B7799FKWT_sku2634230

---
+++ Sasha_code/Samsung NZ64B7799FKWT_sku2634230/app/js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },    
+    
+});
