e0bb5438c6437479ec60f108bcfca067931587d5
Ткаченко Карина Олексіївна <dyukovakarina@gmail.com>
2023-11-14 15:21:47 +0200

    14.11.2023

---
+++ Karina_code/Наушники вкладыши беспроводные TWS Samsung Buds FE White (SM-R400NZWASEK)/app/js/script.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.box-samsungbuds__slider', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // autoHeight: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    // effect: 'fade',
+    // fadeEffect: {
+    //     crossFade: true
+    // },
+    // breakpoints: {
+    //     // when window width is >= 320px
+    //     320: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20
+    //     },
+    //     // when window width is >= 480px
+    //     480: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 30
+    //     },
+    //     // when window width is >= 640px
+    //     640: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 40
+    //     }
+    // }
+});
