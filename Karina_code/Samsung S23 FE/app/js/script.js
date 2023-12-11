87603d4c8476cbf9b5593034679138493f3bd4fd
Ткаченко Карина Олексіївна <dyukovakarina@gmail.com>
2023-12-11 16:08:51 +0200

    11.12.2023

---
+++ Karina_code/Samsung S23 FE/app/js/script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.coloros-samsung23fe__slider', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+});
