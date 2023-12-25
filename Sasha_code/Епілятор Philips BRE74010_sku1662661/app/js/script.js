1f16bebc351b2582ff1003a5474611123c7b5afc
Серкес Олександра Василівна <SerkesOV@comfy.ua>
2023-12-25 14:46:16 +0200

    25/12/2023
    Епілятор Philips BRE74010_sku1662661

---
+++ Sasha_code/Епілятор Philips BRE74010_sku1662661/app/js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },    
+    
+});
